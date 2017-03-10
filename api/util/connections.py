f39838697f0e0b39750d13a4e583ca9ab310b876
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-03-10 11:52:27 -0600

    :boom: quick-checker for web resources

---
+++ api/util/connections.py
@@ -0,0 +1,19 @@
+import requests
+
+
+def is_reachable(url, timeout=0.001, allow_redirects=True, n_tries=3):
+    """
+    Determines if the provided URL is reachable
+
+    :param url: URL to test
+    :param timeout: Seconds to wait before failing (should be small)
+    :param allow_redirects: If 3xx code shouldn't be treated as the final code
+    :param n_tries: Max number of times to retry connection before fail
+    :return: bool
+    """
+    for _ in range(n_tries):
+        resp = requests.head(url, timeout=timeout,
+                             allow_redirects=allow_redirects)
+        if resp.status_code == 200:
+            return True
+    return False
