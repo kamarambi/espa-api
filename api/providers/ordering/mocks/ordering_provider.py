e106eb99f97d2eacc8ccc5b989f1404c5d02f600
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-05-22 14:53:07 -0500

    TST: mock fetch_user_orders

--- api/providers/ordering/mocks/ordering_provider.py
+++ api/providers/ordering/mocks/ordering_provider.py
@@ -1,5 +1,5 @@
 class MockOrderingProvider(object):
 
-    def fetch_user_orders(self, userid, filters={}):
+    def fetch_user_orders(self, username='', email='', filters={}):
         orders = ['1', '2']
-        return {'orders': orders}
+        return orders
