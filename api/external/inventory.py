f27478bb821570e4fde5721189755718a6000c60
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-06-16 09:38:24 -0500

    MAINT: download resource updated
    * The M2M will now return the mapping for entityId to URL

--- api/external/inventory.py
+++ api/external/inventory.py
@@ -288,9 +288,8 @@ class LTAService(object):
             if not isinstance(results, list):
                 raise LTAError('{} failed fetch download urls: {}'
                                .format(sensor_name, product_ids))
-            urls = {i: [r.replace(self.ehost, self.ihost) for r in results
-                        if entity_ids.get(i) in r] for i in id_list}
-            urls = {i: v[0] for i, v in urls.items() if len(v) == 1}
+            urls = {i['entityId']: i['url'].replace(self.ehost, self.ihost)
+                    for i in results}
             diff = set(id_list) - set(urls)
             if diff:
                 raise LTAError('No download urls found for: {}'.format(diff))
