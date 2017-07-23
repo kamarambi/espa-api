d2c23e65c76c9050d764f35e65c0cb1655fc1466
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-07-23 15:34:08 -0500

    TST: more db configs

--- setup/espadev_espa_unit_test_configuration.sql
+++ setup/espadev_espa_unit_test_configuration.sql
@@ -1,8 +1,9 @@
 set search_path = espa_unit_test;
 
 INSERT INTO ordering_configuration (key, value) VALUES
-    ('url.dev.orderservice', ''),
-    ('url.dev.orderupdate', ''),
-    ('url.dev.orderdelivery', ''),
-    ('url.dev.registration', '')
+    ('url.dev.orderservice', 'http://host.com'),
+    ('url.dev.orderupdate', 'http://host.com'),
+    ('url.dev.orderdelivery', 'http://host.com'),
+    ('url.dev.registration', 'http://host.com'),
+    ('url.dev.internal_cache', 'hostname')
 ;
