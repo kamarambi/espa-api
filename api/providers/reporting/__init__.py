d871efb5ab9ead8f70580f754690d4b51898bbde
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-07-31 11:54:32 -0500

    ENH: new 1hr stats endpoints

--- api/providers/reporting/__init__.py
+++ api/providers/reporting/__init__.py
@@ -296,6 +296,28 @@ STATS = {
                      FROM ordering_scene s
                      WHERE s.status = 'onorder' '''
     },
+    'stat_products_complete_1_hrs_sensor': {
+        'display_name': 'Products Complete 1hrs - Sensor',
+        'description': 'Number of products completed last 1 hrs, sorted by Sensor',
+        'query': r'''SELECT COUNT(*) "statistic",
+                            CASE WHEN split_part(s.name, '_', 2) = '' THEN
+                                CASE WHEN s.sensor_type = 'landsat' THEN left(s.name, 3) ELSE left(s.name, 7) END
+                                ELSE left(s.name, 4) END "sensor"
+                     FROM ordering_scene s
+                     WHERE s.status = 'complete'
+                     AND completion_date > now() - interval '1 hours'
+                     GROUP BY "sensor" '''
+    },
+    'stat_products_complete_1_hrs_machine': {
+        'display_name': 'Products Complete 1hrs - Machine',
+        'description': 'Number of products completed last 1 hrs, sorted by Machine',
+        'query': r'''SELECT COUNT(*) "statistic",
+                            s.processing_location "machine"
+                     FROM ordering_scene s
+                     WHERE s.status = 'complete'
+                     AND completion_date > now() - interval '1 hours'
+                     GROUP BY "machine" '''
+    },
 }
 
 
