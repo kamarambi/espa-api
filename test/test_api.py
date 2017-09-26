84a95b08f77f90d4f83478d04bdd5692fcb55b75
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-09-26 11:12:06 -0500

    TST: add bad tests for pixel_units

--- test/test_api.py
+++ test/test_api.py
@@ -242,6 +242,34 @@ class TestValidation(unittest.TestCase):
                 with self.assertRaisesRegexp(exc_type, err_message):
                     api.validation.validate(invalid_order, self.staffuser.username)
 
+    def test_projection_units_geographic(self):
+        """
+        Make sure Geographic (latlon) projection only accepts "dd" units
+        """
+        part_order = {
+            "olitirs8_collection": {
+                "inputs": ['lc08_l1tp_015035_20140713_20170304_01_t1'],
+                "products": ["l1"]
+            },
+            "projection": {"lonlat": None},
+            "format": "gtiff",
+            "resampling_method": "cc"
+            }
+        bad_parts = {
+            "resize": {"pixel_size": 30, "pixel_size_units": "meters"},
+            "image_extents": {"north": 80, "south": -80,
+                              "east": 170, "west": -170, "units": "meters"},
+            }
+
+        err_msg = '{} units must be in "dd" for projection "lonlat"'
+        exc_type = ValidationException
+
+        for bname in bad_parts:
+            invalid_order = copy.deepcopy(part_order)
+            invalid_order.update({bname: bad_parts.get(bname)})
+            with self.assertRaisesRegexp(exc_type, err_msg.format(bname)):
+                api.validation.validate(invalid_order, self.staffuser.username)
+
     # def test_validate_utm_zone(self):
     #     invalid_order = copy.deepcopy(self.base_order)
     #     invalid_order['projection'] = {'utm': {'zone': 50, 'zone_ns': 'north'}}
