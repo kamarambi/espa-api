6744784da3094e0c4ab295e66b89b1443014f441
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-11-28 23:37:18 -0600

    ENH: rewrite providers.reporting

--- api/interfaces/providers.py
+++ api/interfaces/providers.py
@@ -5,7 +5,7 @@ from api.providers.metrics import MockMetricsProvider, MetricsProvider
 from api.providers.ordering import MockOrderingProvider
 from api.providers.ordering.ordering_provider import OrderingProvider
 from api.providers.production.production_provider import ProductionProvider
-from api.providers.reporting.reporting_provider import ReportingProvider
+from api.providers.reporting import ReportingProvider
 from api.providers.validation import MockValidationProvider
 from api.providers.validation.validictory import ValidationProvider
 from api.providers.configuration.configuration_provider import ConfigurationProvider
