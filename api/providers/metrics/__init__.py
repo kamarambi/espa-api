e412155e90d4534f06f6202eafedc02eb8d6d807
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-11-28 19:29:42 -0600

    ENH: concrete baseclass for metrics provider

--- api/providers/metrics/__init__.py
+++ api/providers/metrics/__init__.py
@@ -1,21 +1,41 @@
 import abc
 
+from api.providers.metrics.metrics_provider import Metrics
 
 class MetricsProviderInterface(object):
     __metaclass__ = abc.ABCMeta
 
     @abc.abstractmethod
-    def collect(self, order):
+    def list_metrics(self):
+        pass
+
+    @abc.abstractmethod
+    def read_metric(self, name, **kwargs):
+        pass
+
+    @abc.abstractproperty
+    def metrics():
         pass
 
 
 class MockMetricsProvider(MetricsProviderInterface):
 
-    def collect(self, order):
+    def list_metrics(self):
+        pass
+
+    def read_metric(self, name, **kwargs):
         pass
 
 
 class MetricsProvider(MetricsProviderInterface):
+    metrics = Metrics()
 
-    def collect(self, order):
+    def list_metrics(self):
+        return self.metrics.list_metrics(self.metrics.queries)
+
+    def metric_info(self, name):
+        return self.metrics.metric_info(self.metrics.queries.get(name),
+                                        self.metrics.info_keys)
+
+    def read_metric(self, name, **kwargs):
         pass
